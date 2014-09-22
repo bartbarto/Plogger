0ab7fa43e755bdda54940f0fe426365cf4ce67c3
Bart Van Itterbeeck <bartvanitterbeeck@gmail.com>
2014-09-22 10:59:33 +0200

    Initial Commit

---
+++ Plogger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Plogger
+//
+//  Created by Bart Van Itterbeeck on 22/09/14.
+//  Copyright (c) 2014 Bart Van Itterbeeck. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
